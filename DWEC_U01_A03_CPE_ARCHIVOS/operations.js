8b5735b72a19bd1a4db4d5c8585303728ab4219e
JorgeVz01 <jorgebarvaz@gmail.com>
2023-10-31 12:18:47 +0100

    AA

---
+++ DWEC_U01_A03_CPE_ARCHIVOS/operations.js
@@ -0,0 +1,28 @@
+/* Función suma */
+function suma(a, b) {
+	  return a + b;
+};
+
+/* Función resta */
+function resta(a, b) {
+	  return a - b;
+};
+
+function multiplicacion(a, b) {
+    /* TODO */
+    return a*b;
+}
+
+/* Función división */
+function division(a, b) {
+    /* TODO */
+    return a/b;
+}
+
+/* Exportación de funciones */
+module.exports = {
+	  suma,
+	  resta,
+    multiplicacion,
+    division
+}
